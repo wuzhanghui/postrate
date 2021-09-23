e223ab806bb7481f962de8e25fd64a5c610f20ee
wuzhanghui <47000776+wuzhanghui@users.noreply.github.com>
2021-09-23 20:33:24 +0900

    Update winrate.py

--- winrate.py
+++ winrate.py
@@ -5,6 +5,38 @@ from douzero.evaluation.winratemulation import evaluate
 import shutil
 import os.path
 
+from __future__ import division
+
+import math
+import sys
+import requests
+import os
+import zipfile
+import time
+
+
+
+
+def progressbar(cur,total):
+    percent = '{:.2%}'.format(cur / total)
+    sys.stdout.write('\r')
+    sys.stdout.write('[%-50s] %s' % ( '=' * int(math.floor(cur * 50 /total)),percent))
+    sys.stdout.flush()
+    if cur == total:
+        sys.stdout.write('\n')
+
+def setwenjian():
+    #上传文件到服务器
+    filename = 'data'+str(time.time())+'.zip'
+    zip_file = zipfile.ZipFile(filename, 'w')
+    zip_file.write('app.py',compress_type=zipfile.ZIP_DEFLATED)
+    zip_file.close()
+    file = {'file': open(filename,'rb')}
+    r = requests.post('http://[240b:250:280:cb00:8171:63df:dae6:187b]/upload', files=file,data={'mima':'zheshiyigeshenqidemima'})
+    print(r.text)
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         'Dou Dizhu Evaluation')
@@ -92,5 +124,6 @@ if __name__ == '__main__':
 #         for file in files:
 #             print(file)
 #             shutil.copy("./rate/" + file, '/content/gdrive/MyDrive/data/'+ file[:-4]+'/' + file[:-4] + str(time.time()) + '.csv')
-        os.system("python post.py")
+        #os.system("python post.py")
+        setwenjian()
         shutil.rmtree("./rate/")
